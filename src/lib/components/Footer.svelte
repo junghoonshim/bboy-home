8625cb928de18708427264ecdb4b705f96268e2f
junghoonshim <74518608+junghoonshim@users.noreply.github.com>
2024-11-11 23:28:47 +0900

    first commit

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,11 @@
+<footer>
+    <small>Copyright B-Boy Brothers. All Right reseved.</small>
+</footer>
+
+<style lang="scss">
+    footer {
+        text-align: center;
+        margin-top: 50px;
+    }
+
+</style>
---
+++ src/lib/components/Home.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Footer from "./Footer.svelte";
+    import Header from "./Header.svelte";
+</script>
+<Header />
+<main class="container">
+    <video
+        autoplay loop muted 
+        src="/video/title.mp4"></video>
+</main>
+<Footer />
+<style>
+
+</style>
